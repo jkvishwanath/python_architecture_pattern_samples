7fe8f50d8d863098a62557dd18a6ee55d562a2f0
jk vishwa <jkashivishwanath5@gmail.com>
2021-02-27 17:31:33 +0530

    code changes for domain model

--- src/domain/model.py
+++ src/domain/model.py
@@ -4,19 +4,6 @@ from datetime import date
 from typing import Optional, List, Set
 
 
-class OutOfStock(Exception):
-    pass
-
-
-def allocate(line: OrderLine, batches: List[Batch]) -> str:
-    try:
-        batch = next(b for b in sorted(batches) if b.can_allocate(line))
-        batch.allocate(line)
-        return batch.reference
-    except StopIteration:
-        raise OutOfStock(f"Out of stock for sku {line.sku}")
-
-
 @dataclass(frozen=True)
 class OrderLine:
     orderid: str
@@ -68,3 +55,17 @@ class Batch:
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+
+
+class OutOfStock(Exception):
+    pass
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
+
